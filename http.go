fdfe27639e488709e4fd67f445161c57929148b1
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-01-26 19:53:04 +0800

    feat: opt code

--- http.go
+++ http.go
@@ -14,25 +14,22 @@ const (
 
 // Post 请求
 func Post(urlStr string, reqParam interface{}) ([]byte, error) {
-	reqByte, _ := json.Marshal(reqParam)
-	resp, err := http.Post(urlStr, jsonContentType, bytes.NewBuffer(reqByte))
+	reqByte, err := json.Marshal(reqParam)
 	if err != nil {
-		log.Log().WithField("url", urlStr).
-			WithField("err", err.Error()).
-			Error("Post 请求失败")
+		logError(urlStr, err, "请求参数序列化失败")
 		return nil, err
 	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			return
-		}
-	}(resp.Body)
+
+	resp, err := http.Post(urlStr, jsonContentType, bytes.NewBuffer(reqByte))
+	if err != nil {
+		logError(urlStr, err, "Post 请求失败")
+		return nil, err
+	}
+	defer closeResponseBody(resp.Body, urlStr)
 
 	respByte, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Log().WithField("url", urlStr).
-			WithField("err", err.Error()).Error("读取响应出错")
+		logError(urlStr, err, "读取响应出错")
 		return nil, err
 	}
 	return respByte, nil
@@ -42,28 +39,50 @@ func Post(urlStr string, reqParam interface{}) ([]byte, error) {
 func Get(urlStr string, rsp interface{}) error {
 	resp, err := http.Get(urlStr)
 	if err != nil {
-		log.Log().WithField("url", urlStr).
-			WithField("err", err.Error()).Error("Get 请求失败")
+		logError(urlStr, err, "Get 请求失败")
 		return err
 	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-
-		}
-	}(resp.Body)
+	defer closeResponseBody(resp.Body, urlStr)
 
 	respByte, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Log().WithField("url", urlStr).
-			WithField("err", err.Error()).Error("读取响应出错")
+		logError(urlStr, err, "读取响应出错")
 		return err
 	}
 
 	if err := json.Unmarshal(respByte, rsp); err != nil {
-		log.Log().WithField("url", urlStr).WithField("err", err.Error()).
-			WithField("respByte", respByte).Error("解析响应出错")
+		logError(urlStr, err, "解析响应出错")
 		return err
 	}
 	return nil
+}
+
+// FetchByPost 通过post 获取结果
+func FetchByPost(urlStr string, reqParam interface{}, rsp interface{}) error {
+	respBytes, err := Post(urlStr, reqParam)
+	if err != nil {
+		logError(urlStr, err, "Post 请求失败")
+		return err
+	}
+
+	// 解析参数
+	if err := json.Unmarshal(respBytes, rsp); err != nil {
+		logError(urlStr, err, "解析响应出错")
+		return err
+	}
+	return nil
+}
+
+func logError(url string, err error, message string) {
+	log.Log().WithField("url", url).
+		WithField("err", err.Error()).
+		Error(message)
+}
+
+func closeResponseBody(body io.ReadCloser, url string) {
+	if err := body.Close(); err != nil {
+		log.Log().WithField("url", url).
+			WithField("err", err.Error()).
+			Error("关闭响应体出错")
+	}
 }
