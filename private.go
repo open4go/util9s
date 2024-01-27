97ea6e46312f37a06e19a2fcc781722134b40156
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-01-28 05:16:24 +0800

    feat: update wx conf

---
+++ private.go
@@ -0,0 +1,31 @@
+package util9s
+
+import (
+	"crypto/rsa"
+	"github.com/open4go/log"
+	"github.com/wechatpay-apiv3/wechatpay-go/utils"
+)
+
+type WxConfig struct {
+	PrivateKey *rsa.PrivateKey
+}
+
+var (
+	// GlobalWxConfig 微信配置
+	GlobalWxConfig WxConfig
+)
+
+// InitWxConfig 加载密钥
+func InitWxConfig(path string) (WxConfig, error) {
+	wxc := WxConfig{}
+	// 加载商户私钥
+	mchPrivateKey, err := utils.LoadPrivateKeyWithPath(path)
+	if err != nil {
+		log.Log().WithField("path", path).Error("加载商户密钥失败")
+		return wxc, err
+	}
+	wxc.PrivateKey = mchPrivateKey
+	// 全局
+	GlobalWxConfig = wxc
+	return wxc, nil
+}
