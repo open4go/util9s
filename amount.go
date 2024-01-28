530b67a016de6e64431b42b006c5b014c433a12c
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-01-29 04:22:49 +0800

    feat: update fs

--- amount.go
+++ amount.go
@@ -18,3 +18,12 @@ func FenS2Yuan(amount string) string {
 	}
 	return fmt.Sprintf("%.2f", float64(parseInt)/100)
 }
+
+// FenSF2Yuan 分转元(字符串转字符串）
+func FenSF2Yuan(amount string) string {
+	f, err := strconv.ParseFloat(amount, 64)
+	if err != nil {
+		return amount
+	}
+	return fmt.Sprintf("%.2f", f/100)
+}
