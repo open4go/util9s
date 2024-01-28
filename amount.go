14b5b25d4f8bdc52f689fbe64b9053c38307fa7d
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-01-29 03:31:31 +0800

    feat: add fen string to yuan

--- amount.go
+++ amount.go
@@ -1,8 +1,20 @@
 package util9s
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-// Fen2Yuan 分转元
+// Fen2Yuan 分转元 （整形转字符串）
 func Fen2Yuan(amount int64) string {
 	return fmt.Sprintf("%.2f", float64(amount)/100)
+}
+
+// FenS2Yuan 分转元(字符串转字符串）
+func FenS2Yuan(amount string) string {
+	parseInt, err := strconv.ParseInt(amount, 10, 64)
+	if err != nil {
+		return amount
+	}
+	return fmt.Sprintf("%.2f", float64(parseInt)/100)
 }
